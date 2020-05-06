f4ae53e6462e668b333d160585236ff17a562f49
joepetrowski <joe@parity.io>
2020-05-06 19:18:05 +0200

    change constructor to function

--- index.ts
+++ index.ts
@@ -1,5 +1,19 @@
+//
+import { constructTransaction } from './construction';
+import { UserInputs, TxConstruction } from './util';
+
+const inputs: UserInputs = {
+	senderAddress: '15wAmQvSSiAK6Z53MT2cQVHXC8Z2et9GojXeVKnGZdRpwPvp',
+	recipientAddress: '13iSQm7iyDjoTo3HndhCzpQztAxkNpB1SyRkEuucmAShcApQ',
+	transferValue: 1,
+	tip: 0,
+	validityPeriod: 240,
+	sidecarHost: 'https://cb-runtime-wk8yx7pds0ag.paritytech.net/'
+}
+
 async function main(): Promise<void> {
-	console.log('hello');
+	const construction: TxConstruction = constructTransaction(inputs);
+	console.log(construction.payload);
 }
 
 main().catch((error) => {
--- util.ts
+++ util.ts
@@ -2,7 +2,30 @@
 import { TypeRegistry } from '@polkadot/types';
 import { TRANSACTION_VERSION } from '@polkadot/types/extrinsic/v4/Extrinsic';
 
-import { KeyringPair } from '@substrate/txwrapper';
+import { KeyringPair, UnsignedTransaction } from '@substrate/txwrapper';
+
+export interface UserInputs {
+  // Address sending the transaction.
+  senderAddress: string;
+  // Address receiving the transfer.
+  recipientAddress: string;
+  // Number of tokens to transfer.
+  transferValue: number;
+  // Tip for block producer.
+  tip: number;
+  // Number of blocks for which this transaction is valid.
+  validityPeriod: number;
+  // Host that is running sidecar.
+  sidecarHost: string;
+}
+
+export const DECIMALS = 1_000_000_000_000;
+
+export interface TxConstruction {
+  unsigned: UnsignedTransaction;
+  payload: string;
+  registry: TypeRegistry;
+}
 
 export interface RegistryInfo {
 	chainName: string;
