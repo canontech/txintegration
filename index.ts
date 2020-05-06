5cbf8024bd1d2b7d5e812f20864abb148b8593e8
joepetrowski <joe@parity.io>
2020-05-06 21:19:09 +0200

    get signature

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 //
+import { createSignedTx, getTxHash } from '@substrate/txwrapper';
 import { constructTransaction } from './construction';
 import { UserInputs, TxConstruction } from './util';
+import * as readline from 'readline';
 
 const inputs: UserInputs = {
 	senderAddress: '15wAmQvSSiAK6Z53MT2cQVHXC8Z2et9GojXeVKnGZdRpwPvp',
@@ -11,9 +13,32 @@ const inputs: UserInputs = {
 	sidecarHost: 'https://cb-runtime-wk8yx7pds0ag.paritytech.net/'
 }
 
+function promptSignature(): Promise<string> {
+	let rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	});
+
+	return (new Promise((resolve) => {
+		rl.question('\nSignature: ', (answer) => {
+			resolve(answer);
+			rl.close();
+		})
+	}));
+}
+
 async function main(): Promise<void> {
 	const construction: TxConstruction = await constructTransaction(inputs);
-	console.log(construction.payload);
+	const registry = construction.registry;
+	console.log(`\nSigning Payload: ${construction.payload}`);
+
+	const signature = await promptSignature();
+
+	const tx = createSignedTx(construction.unsigned, signature, { registry });
+	console.log(`\nEncoded Transaction: ${tx}`);
+
+	const exptectedTxHash = getTxHash(tx);
+	console.log(`\nExpected Tx Hash: ${exptectedTxHash}`);
 }
 
 main().catch((error) => {
