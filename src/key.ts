37f0313e0438aa9632fed368c85568cc26afe34e
joe petrowski <25483142+joepetrowski@users.noreply.github.com>
2020-06-04 14:08:09 +0200

    remove double exports

--- src/key.ts
+++ src/key.ts
@@ -1,8 +1,4 @@
-// SR-1
+// Alice
 export const curve = 'sr25519';
 export const signingKey = '//Alice';
 export const senderAddress = '15oF4uVJwmo4TdGW7VfQxNLavjCXviqxT9S1MgbjMNHr6Sp5';
-// SR-2
-export const curve = 'sr25519';
-export const signingKey = '//Bob';
-export const senderAddress = '14E5nqKAp3oAJcmzgZhUD2RcptBeUBScxKHgJKU4HPNcKVf3';
